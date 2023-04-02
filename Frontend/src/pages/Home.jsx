a5681b67a23a914120430a9bde5b204b285e7751
Riyan <thevaluables04@gmail.com>
2023-04-02 11:10:53 +0530

    first commit

---
+++ Frontend/src/pages/Home.jsx
@@ -0,0 +1,48 @@
+import React from 'react'
+import { motion, AnimatePresence } from 'framer-motion';
+import { useSnapshot } from 'valtio'
+import state from '../store';
+import { CustomButton } from '../components';
+import {
+  headContainerAnimation,
+  headContentAnimation,
+  headTextAnimation,
+  slideAnimation
+} from '../config/motion'
+
+const Home = () => {
+  const snap = useSnapshot(state)
+  return (
+    <AnimatePresence>
+      {snap.intro && (
+        <motion.section className="home" {...slideAnimation('left')}>
+          <motion.header {...slideAnimation("down")}>
+            <img 
+              src="./threejs.png"
+              alt="logo"
+              className="w-8 h-8 object-contain"
+            />
+          </motion.header>
+          <motion.div className="head-content" {...headContainerAnimation}>
+            <motion.div {...headTextAnimation}>
+              <h1 className="head-text">
+                LET'S <br className="xl:block hidden" /> CREATE.
+              </h1>
+            </motion.div>
+            <motion.div className="flex flex-col gap-5" {...headContentAnimation}>
+              <p className="max-w-md font-normal text-gray-600 text-base">Create your own design on a 3D interactive canvas and customize your shirts the way you want | <strong> Powered By AI</strong></p>
+              <CustomButton 
+                type="filled"
+                title="Create"
+                handleClick={()=> state.intro = false}
+                customStyles="w-fit px-4 py-2.5 font-bold text-small"
+              />
+            </motion.div>
+          </motion.div>
+        </motion.section>
+      )}
+    </AnimatePresence>
+  )
+}
+
+export default Home
